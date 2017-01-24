d768b7170a08dec0e85dc84d122a712cccdbbff0
Judah Lamm <Judah.Lamm@gmail.com>
2017-01-24 11:22:03 -0600

    Connect the form the sms sending functionality

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const SMS = require('../utilites/sms.js');
 
 router.get('/', (ignore, res) => {
     res.status(200).render("index.pug")
 });
 
 router.post('/api', (req, res) => {
-    res.status(200).send("api hit")
+    const spec = {
+        to: req.body.phone,
+        message: req.body.message
+    };
+
+    SMS.sendSms(spec)
+        .then((data) =>{
+            console.log("data", data);
+            res.status(200).send("api hit")
+        })
+        .catch((err) => {
+            res.send("err");
+        })
 });
 
 module.exports = router
