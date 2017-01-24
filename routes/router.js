35fd0443cc83c9158fe6c96dc90393e19b3ac267
Judah Lamm <Judah.Lamm@gmail.com>
2017-01-24 12:51:29 -0600

    Add route and logic to sms message url and load up the message

--- routes/router.js
+++ routes/router.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const SMS = require('../utilites/sms.js');
+const Messages = require('../utilites/messages.js');
 
 router.get('/', (ignore, res) => {
     res.status(200).render("index.pug")
 });
 
 router.post('/api', (req, res) => {
+    const body = req.body;
     const spec = {
-        to: req.body.telephone,
-        message: req.body.message
+        email: body.email,
+        phoneNumber: body.telephone,
+        message: body.message
     };
 
-    SMS.sendSms(spec)
+    Messages.saveMessage(spec)
         .then((data) =>{
             console.log("data", data);
             res.status(200).send("api hit")
@@ -20,7 +22,21 @@ router.post('/api', (req, res) => {
         .catch((err) => {
             console.log("err", err);
             res.send(err.code);
+        });
+});
+
+router.get('/messages/:id', (req, res) => {
+    const messageId = req.params.id;
+
+    Messages.findMessage(messageId)
+        .then((data) =>{
+            console.log("data", data);
+            res.status(200).send(data)
         })
+        .catch((err) => {
+            console.log("err", err);
+            res.send(err.code);
+        });
 });
 
 module.exports = router
---
+++ utilites/messages.js
@@ -0,0 +1,35 @@
+"use strict"
+
+const Messages = require('../models/').Message;
+const SMS = require('./sms.js');
+
+const saveMessage = (spec) => {
+    return Messages.create({
+        email: spec.email,
+        phone_number: spec.phoneNumber,
+        message: spec.message
+    })
+    .then((data) => {
+        console.log("data", data);
+        const messageUrl = `localhost:3000/messages/${data.id}`;
+        const phoneNumber = data.phone_number;
+        const email = data.email;
+
+        //check for email and send email instead
+        return SMS.sendSms(data.phone_number, messageUrl)
+    });
+};
+
+
+const findMessage = (messageId) => {
+    return Messages.findById(messageId)
+        .then((message) => message.message)
+};
+
+
+module.exports = {
+    saveMessage,
+    findMessage
+    }
+
+
