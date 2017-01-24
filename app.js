6bcc0b7429c8d3fbc4dd05737eb3bc31f5f665b4
Judah Lamm <Judah.Lamm@gmail.com>
2017-01-24 10:19:57 -0600

    Add a basic router to the server

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 "use strict";
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const routes = require('./routes/index.js')
+
 let server;
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/templates/views'));
 
-app.use('/', (req, res) => res.send("ok"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
+app.use('/', routes);
+
+app.use(function (ignore, res) {
+    res.status(404).render("404/index.pug");
+});
 
 server = app.listen(process.env.app_port || 3000, function () {
     const host = server.address().address;
