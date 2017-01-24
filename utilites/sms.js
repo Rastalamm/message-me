7c2808b540a848805b46ec5508f09ab97ef0c642
Judah Lamm <Judah.Lamm@gmail.com>
2017-01-24 11:51:57 -0600

    Add sending of sms to valid input phone number

--- utilites/sms.js
+++ utilites/sms.js
@@ -1,26 +1,42 @@
 "use strict"
 
 const twilioConfig = require('../config.js').twilio;
-const promise = require('bluebird');
+const Promise = require('bluebird');
 const client = require('twilio')(twilioConfig.sid, twilioConfig.api);
+
+const phone = require('phone');
+
+const formatPhoneNumber = (number) => {
+    const formattedNumber = phone(number);
+
+    if (formattedNumber.length !== 2) {
+        throw {code:400, message: 'Invalid phone number'}
+    }
+
+    return formattedNumber[0];
+};
 
 const sendSms = (spec) => {
 
     console.log("sms", spec);
 
-    return client.messages.create({
-        to:spec.to,
-        from: twilioConfig.phoneNumber,
-        body: spec.message
-    })
-    .then((response) => {
-        console.log(response.from);
-        console.log(response.body);
-        return response;
-    })
-    .catch((error) => {
-        console.log("erorr", error);
-    })
+    return Promise.try(formatPhoneNumber)
+        .then((number) => {
+           return client.messages.create({
+               to:to,
+               from: twilioConfig.phoneNumber,
+               body: spec.message
+           })
+           .then((response) => {
+               console.log(response.from);
+               console.log(response.body);
+               return response;
+           })
+           .catch((error) => {
+               console.error("error", error);
+               throw {code: 500, message: error.body}
+           })
+        })
 };
 
 module.exports = {sendSms}
